656345753d42ebfc02c48e5d088bb8576c27130a
DMpablo <phdemarco@gmail.com>
2022-09-12 22:59:20 -0300

    strated game-numeric

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+    primary:'#B25068',
+    secondary:'#774360',
+    text:'#FFE6E6',
+    white:'#FFF2F2',
+    gray:'#E0D8B0',
+    black: '#000000',
+    background:'#E7AB79',
+    backgroundDark:'#4C3A51',
+    backgroundLight:'#4C3A51',
+}
+
---
+++ screens/start-games.js
@@ -0,0 +1,91 @@
+import React, { useState} from "react";
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } from "react-native";
+import Card from "../components/card";
+import Input from "../components/input";
+import { colors } from "../constants/colors";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    title: {
+        fontSize: 20,
+        color: colors.secondary,
+        textAlign: 'center',
+        paddingVertical: 20,
+    },
+    label: {
+        fontSize: 14,
+        color: colors.backgroundDark,
+        textAlign: 'center',
+        paddingVertical: 5, 
+    },
+    inputContainer: {
+        width: 320,
+        maxWidth: '80%',
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+    },
+    input: {
+        width: '100%',
+        borderBottomColor: colors.secondary,
+        borderBottomWidth: 1,
+        maxWidth: 70,
+        fontSize: 25,
+        paddingVertical: 10,
+        textAlign: 'center',
+    },
+    buttonContainer: {
+        width: '75%',
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+    }
+});
+
+const StartGameScreen = () => {
+    const [number, setNumber] = useState('');
+
+    const onHandleChange = (text) => {
+        setNumber(text.replace(/[^0-9]/g, ''));
+    }
+    return (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.container}>
+            <Text style={styles.title}>Comenzar juego</Text>
+            <Card style={styles.inputContainer}>
+                <Text style={styles.label}>Elija un numero</Text>
+                <Input 
+                    style={styles.input} 
+                    keyboardType='numeric' 
+                    maxLength={2}
+                    blurOnSubmit
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    onChangeText={(text) => onHandleChange(text)}
+                    value={number}
+                />
+                <View style={styles.buttonContainer}>
+                    <Button
+                        title="Limpiar"
+                        onPress={() => null}
+                        color={colors.primary}
+                    />
+                    <Button
+                        title="Confirmar"
+                        onPress={() => null}
+                        color={colors.secondary}
+                    />
+                </View>
+            </Card>
+        </View>
+        </TouchableWithoutFeedback>
+    )
+}
+
+export default StartGameScreen;
